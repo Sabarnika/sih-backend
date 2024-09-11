d8f7db01b290f5c56977771c8f8cdeb10996235c
Sabarnika S <98590604+Sabarnika@users.noreply.github.com>
2024-09-11 20:11:52 +0530

    Delete expenseSchema.js

--- expenseSchema.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-const expenseSchema = new mongoose.Schema({
-  userId: { type:String, required: true }, 
-  reason: { type: String, required: true },
-  amount: { type: Number, required: true },
-  date: { type: Date, required: true },
-});
-module.exports = mongoose.model("Expense", expenseSchema);
